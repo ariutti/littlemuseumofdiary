c28e1c21104c2012a0628ddb4e6c9c792bc637af
ariutti <nicola.ariutti@gmail.com>
2019-07-02 18:15:53 +0200

    modifiche al codice arduino lettura sharp distance analogico

--- Arduino/sharp_distance/sharp_distance.ino
+++ Arduino/sharp_distance/sharp_distance.ino
@@ -3,14 +3,23 @@ float voltage = 0.0;
 
 // filter stuff nad variables
 float filtered = 0.0;
-float A = 0.1;
+float A = 0.07;
 float B;
 
+float linearizedDistance = 0.0;
+float prevLinearizedDistance = 0.0;
+
+long lastTime = 0;
+
+
+// SETUP ///////////////////////////////////////////////////////////////////////////////
 void setup() {
   Serial.begin(9600);
+  Serial.println("\n BEGIN \n");
   B = 1.0 - A;
 }
 
+// LOOP ////////////////////////////////////////////////////////////////////////////////
 void loop() {
   float raw = analogRead( SHARP );
   // 5V / 1024 steps = 4,9mV per step (circa)
@@ -23,7 +32,27 @@ void loop() {
   
   //Serial.print( raw );Serial.println("\t");
   //Serial.print( voltage );Serial.println("\t");
-  Serial.print( filtered );Serial.println("\t");
-  delay(50);
+  //Serial.print( filtered );Serial.println("\t");
 
+  // source: https://acroname.com/articles/linearizing-sharp-ranger-data
+  if(raw > 0.0 )
+  {
+    linearizedDistance = (6787.0/(filtered - 3)) - 4;
+    //filtered=A*linearizedDistance + B*prevLinearizedDistance;
+    //prevLinearizedDistance = filtered;
+  }
+
+  //Serial.print( linearizedDistance );Serial.println("\t");
+
+  //Serial.print("[ ");
+  if( millis() - lastTime > 1000) {
+    Serial.print( raw );Serial.print("\t");
+    Serial.print( filtered );Serial.print("\t");
+    Serial.print( voltage ); Serial.print("\t");
+    Serial.print( linearizedDistance ); Serial.print("\t");
+    Serial.println();
+    lastTime = millis();
+  }
+  
+  delay(25);
 }
